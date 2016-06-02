b77d904538abf6f04c7a5df14c7384b4e37f65a9
KaroTomSzym <ulepszony.golem@gmail.com>
2016-06-02 17:20:54 +0200

    cd.

---
+++ PolygonCollisionMT/Road.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace PolygonCollisionMT
+{
+    public class Road
+    {
+        private List<Polygon> _polygons;
+        public List<Polygon> VisiblePolygons { get; set; }
+        public Area VisibleArea { get; set; }
+
+        public Road()
+        {
+            VisibleArea = new Area(300,400);
+        }
+
+    }
+}
